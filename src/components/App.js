c89b4abc521c6bc8c66ac06bd06081c453f824ad
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-29 14:59:55 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
+import axios from "axios";
 
 const App = () => {
+
+  let [data, setData] = useState("")
+
+
+  axios
+     .get("https://dummyjson.com/products")
+     .then((response) => setData(response.data))
+     .catch((err) => console.log(err));
+
+     let ans = JSON.stringify(data)
+
+
   return (
-    <div>
+     <div>
         {/* Do not remove the main div */}
-    </div>
-  )
+        <h1>Data Fetched from API</h1>
+        {
+          data && 
+          <pre>{ans}</pre>
+        }
+        {
+          !data &&
+          <p>Loading...</p>
+        }
+        {/* <p>Hello</p> */}
+
+        {/* {
+          arr.map((e, i)=>)
+        } */}
+     </div>
+  );
 }
 
 export default App
